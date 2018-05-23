d644beab5bceaf6eed69d339df186fc96d14007e
Michael Durfey <mdurfey@me.com>
2018-05-22 18:00:04 -0700

    added hot module reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,33 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: [
+    'react-hot-loader/patch',
     path.join(__dirname, '/src/index.js'),
   ],
+  module: {
+    rules:[
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
   output: {
     path: path.resolve(__dirname, '/dist'),
     publicPath: '/',
     filename: 'bundle.js'
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
   }
 }
